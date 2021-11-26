b5bd77724e9f15658d8661a5d8bb370db68807e5
Dasty12 <56167154+Dasty12@users.noreply.github.com>
2021-11-26 16:21:39 +0100

    ...

--- PWM generator/PWM.sv
+++ PWM generator/PWM.sv
@@ -1,8 +1,8 @@
-module PWM #(parameter KPERIOD = 50, parameter KCLKDIV = 1000)
+module PWM #(parameter KPERIOD = 50, parameter KCLKDIV = 10)
 (
     input clk,
     input rst,
-    input [$clog2(KPERIOD)-1:0]CMPA,    //period register
+    input [$clog2(KPERIOD)-1:0] CMPA,    //period register
     output PWM_OUT
 );
 
@@ -14,8 +14,9 @@ reg PWM_OUT_l;              //PWM OUT LOCAL
 
 always @(posedge clk) begin
     if(rst) begin
-        clk_pwm <= 0;
+        clk_pwm <= 1;
         clk_pwm_cnt <= 0;
+        
     end
     else begin
         if(clk_pwm_cnt < KCLKDIV)begin
@@ -31,6 +32,7 @@ end
 always @(posedge clk_pwm) begin
     if(rst) begin
         TBCTR <= 0;
+        PWM_OUT_l <= 0;
     end
     else begin
         if(TBCTR < KPERIOD) begin
--- PWM generator/PWM_tb.sv
+++ PWM generator/PWM_tb.sv
@@ -2,26 +2,38 @@
 `timescale 1ns/1ns
 
 module PWM_tb;
-
-reg clk;
+localparam KPERIOD_PWM =  1000;
+reg clk = 0;
 reg rst;
-reg [5:0] CMPA;
+reg [$clog2(KPERIOD_PWM)-1:0] CMPA;
 wire PWM_OUT;
+reg rst_cnt = 0;
 
-PWM pwm(
+
+
+PWM #(.KPERIOD(KPERIOD_PWM),.KCLKDIV(10))
+    pwm(
     .clk(clk),
     .rst(rst),
     .CMPA(CMPA),
     .PWM_OUT(PWM_OUT)
-);
+    );
+
+
+always @(posedge clk) begin
+    if(rst_cnt == 0) begin
+        rst_cnt <= rst_cnt + 1;
+        rst = 1;
+    end
+    else begin
+      rst = 0;  
+    end
+end
 
 always #5 clk =!clk;
 
 initial begin
-    clk = 0;
-    #5 rst = 1;
-    #5 rst = 0;
-    CMPA = 20;
+    CMPA = 200;
 
     $dumpfile("PWM.vcd");
     $dumpvars;
