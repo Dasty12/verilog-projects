b37dbeb52b920e28cd9e38e1338b06c0c45e8cd9
Jenší David <david.jensi@skoda.cz>
2021-12-24 10:10:20 +0100

    Uart read tested
    readed byte i send to output

--- library/CPUs/UART_cpu.sv
+++ library/CPUs/UART_cpu.sv
@@ -9,7 +9,7 @@ module UART_cpu (
 reg c0;
 wire locked;
 
-reg[7:0] dataTx_memory[10];
+reg[7:0] dataTx_memory[11];
 reg[3:0] dataTx_cnt;
 reg[7:0] dataTx;
 reg[7:0] dataRx;
@@ -41,6 +41,7 @@ initial begin
     for(i = 0;i<10;i++)begin
         dataTx_memory[i] <= 48 + i;
     end
+    dataTx_memory[10] = 10;
 end
 
 
@@ -63,21 +64,20 @@ UartTop device(.clk(c0),
                .out_Rx_ORE(out_Rx_ORE));
 
 
-always @(posedge clk) begin
+always @(posedge c0) begin
 
     if (UserBtn_rise) begin
         valid  <= 1;
-        dataTx <= dataTx_memory[dataTx_cnt];
-        if( dataTx_cnt < 9) begin
-            dataTx_cnt <= dataTx_cnt + 1;
+        //dataTx <= dataTx_memory[dataTx_cnt];
+        if( dataTx_cnt < 10) begin
+    //        dataTx_cnt <= dataTx_cnt + 1;
         end else begin
-            dataTx_cnt <= 0;
+          //  dataTx_cnt <= 0;
         end
     end else begin
         valid  <= 0;
     end
-
-   // dataTx <= 56;   
+ 
     if(smitt_butt_field == 5'b11111) begin  
         UserBtn_fil <= 1;
     end else begin
@@ -94,6 +94,10 @@ always @(posedge clk) begin
     UserBtn_fil_old <= UserBtn_fil;
 
 
+
+    if(out_RXNE == 1) begin
+        dataTx <= dataRx;
+    end
 
 
 end
