5706d52ecc59c367235517edda4c1350a359e9b2
Jenší David <david.jensi@skoda.cz>
2022-01-21 15:21:43 +0100

    ...

--- wishbone/UartCoder.sv
+++ wishbone/UartCoder.sv
@@ -6,5 +6,15 @@ module UartCoder (
     input [33:0] i_word,
     output [7:0] out_char
 );
+
+
+always@(posedge i_clk) begin
+	if(i_word[33:32] == 2'b0) begin//todo read instrukce 
+			//čte se, dokud neni zakoncovaci znak nebo jina instrukce
+	end
+end
+
     
 endmodule
+
+//pokud je read(1.0000), tak se prenesou horni 2 ridici bity a zbytek jsou ctena data
