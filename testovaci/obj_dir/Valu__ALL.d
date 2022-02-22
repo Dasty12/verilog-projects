4b95dd82c86f14dc17c0e6cc437f2ffc3da0d79c
Dasty12 <56167154+Dasty12@users.noreply.github.com>
2022-02-22 22:01:34 +0100

    ...

---
+++ testovaci/obj_dir/Valu__ALL.d
@@ -0,0 +1,14 @@
+Valu__ALL.o: Valu__ALL.cpp Valu.cpp Valu.h \
+ C:/msys64/mingw64/share/verilator/include/verilated.h \
+ C:/msys64/mingw64/share/verilator/include/verilatedos.h \
+ C:/msys64/mingw64/share/verilator/include/verilated_types.h \
+ C:/msys64/mingw64/share/verilator/include/verilated_funcs.h Valu__Syms.h \
+ Valu___024root.h Valu___024unit.h \
+ C:/msys64/mingw64/share/verilator/include/verilated_vcd_c.h \
+ C:/msys64/mingw64/share/verilator/include/verilated.h \
+ C:/msys64/mingw64/share/verilator/include/verilated_trace.h \
+ C:/msys64/mingw64/share/verilator/include/verilated_trace_defs.h \
+ Valu___024root__DepSet_ha59b247d__0.cpp Valu__Trace__0.cpp \
+ Valu___024root__Slow.cpp Valu___024root__DepSet_ha59b247d__0__Slow.cpp \
+ Valu___024unit__Slow.cpp Valu___024unit__DepSet_h45503383__0__Slow.cpp \
+ Valu__Syms.cpp Valu__Trace__0__Slow.cpp
---
+++ testovaci/obj_dir/tb_alu.d
@@ -0,0 +1,10 @@
+tb_alu.o: tb_alu.cpp \
+ C:/msys64/mingw64/share/verilator/include/verilated.h \
+ C:/msys64/mingw64/share/verilator/include/verilatedos.h \
+ C:/msys64/mingw64/share/verilator/include/verilated_types.h \
+ C:/msys64/mingw64/share/verilator/include/verilated_funcs.h \
+ C:/msys64/mingw64/share/verilator/include/verilated_vcd_c.h \
+ C:/msys64/mingw64/share/verilator/include/verilated.h \
+ C:/msys64/mingw64/share/verilator/include/verilated_trace.h \
+ C:/msys64/mingw64/share/verilator/include/verilated_trace_defs.h Valu.h \
+ Valu___024unit.h
